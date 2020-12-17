9f5f9bf53c1586cff563e115c0c8fa9ce2ed1992
jagriti04 <jagriti.agni@gmail.com>
2020-12-17 13:39:56 +0530

    [Jagriti]ADDED: Payroll form jsx file and css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
-import PayrollForm from "../";
+import PayrollForm from "./components/payroll_form/payroll_form";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <PayrollForm />
+      </div>
+    );
+  }
 }
