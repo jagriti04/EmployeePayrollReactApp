5427c7a23358f8a3572b20e681cb877721b2326d
jagriti04 <jagriti.agni@gmail.com>
2020-12-17 09:09:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-v1voyp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-v1voyp)
---
+++ src/components/payroll_form/payroll_form.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+class PayrollForm extends React.Component {}
+
+export default PayrollForm;
